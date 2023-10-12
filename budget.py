42ea1a138889992ba1bf7e31489305190eca71c2
Kinoruu <52972584+Kinoruu@users.noreply.github.com>
2023-10-12 14:30:21 +0000

    Base of class done

--- budget.py
+++ budget.py
@@ -1,5 +1,44 @@
 class Category:
+  def __init__(self, name):
+    self.name = name
+    self.ledger = []
+    self.balance = 0
 
+  def deposit(self, amount, description = ""):
+    #apped to ledger
+    self.ledger.append({
+      "amount": amount,
+      "description": description,
+      })
+  
+  def withdraw(self, amount, description = ""):
+    if not self.check_funds(amount):
+            return False
+  
+    self.balance -= amount
+    self.ledger.append({
+      'amount': -amount,
+      'description': description,
+      })
+
+    return True
+
+  def get_balance(self):
+    return self.balance
+    
+  def transfer(self, amount, destination):
+    self.balance -= amount
+    destination.balance += amount
+    #print("Transfer to [Destination Budget Category]")
+  
+
+  def check_funds(self, amount):
+    if self.balance < amount:
+        return False
+    return True
+    
+  
+  
  
 
 
